46db949c228b422f14862cecc15e5461793e214b
Christian Brodersen <71077860+CKBrodersen@users.noreply.github.com>
2022-03-22 13:25:02 +0100

    nye ting

--- python/random_colour.py
+++ python/random_colour.py
@@ -3,7 +3,7 @@ import random
 paletteMaker = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'] 
 color = [] 
 while len(color) < 24: 
-    if (col := ''.join(['#', *random.choices(paletteMaker, k = 6)])) not in color: 
+    if (col = ''.join(['#', *random.choices(paletteMaker, k = 6)])) not in color: 
         color.append(col) 
 print(color) 
 t = Turtle() 
